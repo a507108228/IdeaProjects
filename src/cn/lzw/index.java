57b7d855c2c4c776d77e7a01ad829728aa5a8950
lzw <58364878+a507108228@users.noreply.github.com>
2021-01-07 22:10:48 +0800

    Idea 整合 Github 新增加一个 测试

---
+++ src/cn/lzw/index.java
@@ -0,0 +1,14 @@
+package cn.lzw;
+
+/**
+ * @author： lzw
+ * @date： 2021/1/7 22:05
+ * @description：
+ */
+public class index{
+    public static void main(String[] args){
+     System.out.println("你好啊GitHub！！！");
+
+    }
+
+}
