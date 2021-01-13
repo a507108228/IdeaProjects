ce77576c34c45449fa28ed9b83d904b946402c32
lzw <58364878+a507108228@users.noreply.github.com>
2021-01-13 10:57:35 +0800

    Idea 整合 Github 新增加一个 测试

--- src/cn/lzw/index.java
+++ src/cn/lzw/index.java
@@ -1,5 +1,7 @@
 package cn.lzw;
 
+import java.util.Arrays;
+
 /**
  * @author： lzw
  * @date： 2021/1/7 22:05
@@ -8,6 +10,10 @@ package cn.lzw;
 public class index{
     public static void main(String[] args){
      System.out.println("你好啊GitHub！！！");
+     int [] arr1 = new int[]{1,2,3,4};
+     int [] arr2 = new int[]{1,2,5,4};
+     boolean isEquals =Arrays.equals(arr1,arr2 );
+     System.out.println(isEquals);
 
     }
 
