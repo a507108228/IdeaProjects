e8cfcba19861f542c412c0f237e8d32e24d1d116
liziwei <58364878+a507108228@users.noreply.github.com>
2021-05-11 19:21:53 +0800

    Update TestJdbc.java

--- src/main/java/cn/lzw/jdbc/TestJdbc.java
+++ src/main/java/cn/lzw/jdbc/TestJdbc.java
@@ -13,10 +13,11 @@ public class TestJdbc{
     public static void main(String[] args) throws Exception{
         //加载驱动
         Class.forName("com.mysql.jdbc.Driver");
-        //用户信息和url
+        //用户信息和url  这三个可以换一种写法
         String url = "jdbc:mysql://localhost:3306/kkbdb?useUnicode=true&characterEncoding=utf8&useSSL=true";
         String username = "root";
         String password = "201314";
+//      Connection connection = DriverManager.getConnectionn(url:"jdbc:mysql://localhost:3306/kkbdb?useUnicode=true&characterEncoding=utf8&useSSL=true",uesr,"root",password:"201314");
         //连接成功 数据库对象
         Connection connection = DriverManager.getConnection(url,username,password);
         //执行SQL的对象Statement 执行sql对象
